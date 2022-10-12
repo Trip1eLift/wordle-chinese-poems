5486a335f623a1bc9c15b9f257b0f0f6d640b22b
Trip1eLift <josephjbc@gmail.com>
2022-10-12 23:45:16 +0100

    Integrated aws s3 sync into terraform

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,9 +1,14 @@
 # S3 bucket for website.
 resource "aws_s3_bucket" "root_bucket" {
-  bucket = "${var.bucket_name}"
-  policy = templatefile("templates/s3-policy.json", { bucket = "${var.bucket_name}" })
+	bucket = "${var.bucket_name}"
+	policy = templatefile("templates/s3-policy.json", { bucket = "${var.bucket_name}" })
 
-  tags = var.common_tags
+	tags = var.common_tags
+
+	provisioner "local-exec" {
+		when    = destroy
+		command = "aws s3 rm s3://${self.bucket} --recursive"
+	}
 }
 
 resource "aws_s3_bucket_website_configuration" "root_bucket" {
@@ -19,8 +24,8 @@ resource "aws_s3_bucket_website_configuration" "root_bucket" {
 }
 
 resource "aws_s3_bucket_acl" "example_bucket_acl" {
-  bucket = aws_s3_bucket.root_bucket.id
-  acl    = "public-read"
+	bucket = aws_s3_bucket.root_bucket.id
+	acl    = "public-read"
 }
 
 resource "aws_s3_bucket_cors_configuration" "root_bucket" {
@@ -31,5 +36,22 @@ resource "aws_s3_bucket_cors_configuration" "root_bucket" {
 		allowed_methods = ["GET", "POST"]
 		allowed_origins = ["https://${var.domain_name}"]
 		max_age_seconds = 3000
+	}
+}
+
+resource "null_resource" "s3_sync" {
+	depends_on = [
+		aws_s3_bucket.root_bucket
+	]
+
+	triggers = {
+	  always_run = "${timestamp()}"
+	}
+
+	# This script requires aws cli
+	provisioner "local-exec" {
+		command = <<-EOT
+			aws s3 sync ../build s3://${var.bucket_name}
+		EOT
 	}
 }
